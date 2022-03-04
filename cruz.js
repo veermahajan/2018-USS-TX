c869039d226f6651ed459f7367dd747f96037cb1
veermahajan <73316235+veermahajan@users.noreply.github.com>
2022-03-03 17:49:23 -0800

    Update cruz.js

--- cruz.js
+++ cruz.js
@@ -31,20 +31,39 @@ function q2(){
 
 function a2(){
   polling += (Math.floor(Math.random() * 10) - 20)/10;
-  document.getElementById("main").innerHTML = "<p>You are the incumbent!<br>Polling:<br>Cruz: " + polling + "%<br>O'Rourke: " + (100 - polling) + "%</p><br><button onclick='q2()'>Next</button>";
+  document.getElementById("main").innerHTML = "<p>You are the incumbent!<br>Polling:<br>Cruz: " + polling + "%<br>O'Rourke: " + (100 - polling) + "%</p><br><button onclick='q3()'>Next</button>";
 }
 
 function b2(){
   polling += (Math.floor(Math.random() * 10) - 15)/10;
-  document.getElementById("main").innerHTML = "<p>People have already forgotten about it, and that's good because your 180 on Trump since then.<br>Polling:<br>Cruz: " + polling + "%<br>O'Rourke: " + (100 - polling) + "%</p><br><button onclick='q2()'>Next</button>";
+  document.getElementById("main").innerHTML = "<p>People have already forgotten about it, and that's good because your 180 on Trump since then.<br>Polling:<br>Cruz: " + polling + "%<br>O'Rourke: " + (100 - polling) + "%</p><br><button onclick='q3()'>Next</button>";
 }
 
 function c2(){
   polling += (Math.floor(Math.random() * 10) - 20)/10;
-  document.getElementById("main").innerHTML = "<p>This does not satisfy moderates. The only people who hate Beto will vote for you anyway.<br>Polling:<br>Cruz: " + polling + "%<br>O'Rourke: " + (100 - polling) + "%</p><br><button onclick='q2()'>Next</button>";
+  document.getElementById("main").innerHTML = "<p>This does not satisfy moderates. The only people who hate Beto will vote for you anyway.<br>Polling:<br>Cruz: " + polling + "%<br>O'Rourke: " + (100 - polling) + "%</p><br><button onclick='q3()'>Next</button>";
 }
 
 function d2(){
   polling += (Math.floor(Math.random() * 20) - 30)/10;
-  document.getElementById("main").innerHTML = "<p>Good move, though some of your moves have been unpopular, such as your fillibustering.<br>Polling:<br>Cruz: " + polling + "%<br>O'Rourke: " + (100 - polling) + "%</p><br><button onclick='q2()'>Next</button>";
+  document.getElementById("main").innerHTML = "<p>Good move, though some of your moves have been unpopular, such as your fillibustering.<br>Polling:<br>Cruz: " + polling + "%<br>O'Rourke: " + (100 - polling) + "%</p><br><button onclick='q3()'>Next</button>";
+}
+
+function q3(){
+  document.getElementById("main").innerHTML = "<p>What is your ideology?</p><br><button onclick='a3()'>Moderate</button><br><button onclick='b3()'>Established Republican</button><br><button onclick='c3()'>Firebrand conservative</button>";
+}
+
+function a3(){
+  polling += (Math.floor(Math.random() * 20) - 20)/10;
+  document.getElementById("main").innerHTML = "<p>The GOP is quite right wing, and the base is displeased, but this helps with moderates.<br>Polling:<br>Cruz: " + polling + "%<br>O'Rourke: " + (100 - polling) + "%</p><br><button onclick='q3()'>Next</button>";
+}
+
+function b3(){
+  polling += (Math.floor(Math.random() * 10) - 15)/10;
+  document.getElementById("main").innerHTML = "<p>You were an outsider 6 years ago; also, the Republicans are not popular now.<br>Polling:<br>Cruz: " + polling + "%<br>O'Rourke: " + (100 - polling) + "%</p><br><button onclick='q3()'>Next</button>";
+}
+
+function c3(){
+  polling += (Math.floor(Math.random() * 10) - 20)/10;
+  document.getElementById("main").innerHTML = "<p>This does not help with moderate suburbanites.<br>Polling:<br>Cruz: " + polling + "%<br>O'Rourke: " + (100 - polling) + "%</p><br><button onclick='q3()'>Next</button>";
 }
